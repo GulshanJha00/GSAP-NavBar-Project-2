c2b29fa51c62190d485335f6294e8ccecf3fe5b2
Gulshan Kumar <gulshankumar@Gulshans-MacBook-Air.local>
2024-10-13 16:23:16 +0530

    Website done

---
+++ script.js
@@ -0,0 +1,26 @@
+var tl = gsap.timeline();
+var main = document.querySelector("#nav i")
+var cursor = document.querySelector("#full i")
+
+tl.to("#full", {
+    right:0,
+    duration : 0.4,
+})
+tl.from("#full h4", {
+    x:150,
+    opacity:0,
+    duration: 0.5,
+    stagger: 0.3,
+})
+tl.from("#full i", {
+    opacity: 0,
+    x:150,
+    duration:0.5,
+})
+tl.pause()
+main.addEventListener("click", ()=>{
+    tl.play()
+})
+cursor.addEventListener("click",()=>{
+    tl.reverse()
+})
